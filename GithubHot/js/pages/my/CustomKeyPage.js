76fe3dabdc127bec172cafde652dc33cc58692cb
yty <qqwdq>
2017-04-13 22:38:15 +0800

    自定义标签页面，本地存储标签数据

--- GithubHot/js/pages/my/CustomKeyPage.js
+++ GithubHot/js/pages/my/CustomKeyPage.js
@@ -8,14 +8,35 @@ import {
     Text,
     View,
     Image,
-    TouchableOpacity
+    TouchableOpacity,
+    AsyncStorage
 } from 'react-native';
 
 import NavigationBar from '../../component/NavigationBar';
+import CheckBox from 'react-native-check-box';
+import Toast from "react-native-easy-toast";
 
 export default class CustomKeyPage extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            data: [
+                {name: 'Android', checked: true},
+                {name: 'IOS', checked: false},
+                {name: 'React Native', checked: true},
+                {name: 'Java', checked: true},
+                {name: 'JS', checked: true}
+            ]
+        }
+    }
+
     handleBack = () => {
         this.props.navigator.pop();
+    }
+    handleSave = () => {
+        console.log(JSON.stringify(this.state.data));
+        AsyncStorage.setItem('custom_key', JSON.stringify(this.state.data))
+            .then(() => this.refs.toast.show('保存成功'));
     }
     getNavLeftBtn = () => {
         return <View style={{flexDirection:'row',alignItems:'center',paddingLeft: 8}}>
@@ -31,22 +52,65 @@ export default class CustomKeyPage extends Component {
     getNavRightBtn = () => {
         return <View style={{flexDirection:'row',alignItems:'center',paddingRight: 8}}>
             <TouchableOpacity
-                activeOpacity={0.7}>
+                activeOpacity={0.7}
+                onPress={this.handleSave}>
                 <Text style={{fontSize:16,color:'#fff'}}>保存</Text>
             </TouchableOpacity>
         </View>;
     }
 
+    handleClick = (item) => {
+        item.checked = !item.checked;
+    }
+
+    renderCheckBox = (item) => {
+        return item == undefined ? <View style={{flex:1,padding:5}}></View> : <CheckBox
+                style={{flex:1,padding:5}}
+                leftText={item.name}
+                isChecked={item.checked}
+                onClick={()=>this.handleClick(item)}
+                unCheckedImage={<Image source={require('../../../res/images/ic_check_box_outline_blank.png')} style={styles.checkbox}/>}
+                checkedImage={<Image source={require('../../../res/images/ic_check_box.png')} style={styles.checkbox}/>}/>;
+    }
+
+    renderViews = () => {
+        let views = [];
+        for (let i = 0; i < this.state.data.length; i += 2) {
+            views.push((<View key={`view_${i}`} style={{flexDirection:'row'}}>
+                {this.renderCheckBox(this.state.data[i])}
+                {this.renderCheckBox(this.state.data[i + 1])}
+            </View>));
+        }
+
+        return views;
+    }
+
     render() {
         return <View style={styles.container}>
             <NavigationBar title="自定义分类" leftBtn={this.getNavLeftBtn()} rightBtn={this.getNavRightBtn()}/>
-            <Text>CustomKeyPage</Text>
+            <View style={{padding:10}}>
+                {this.renderViews()}
+            </View>
+            <Toast ref="toast"/>
         </View>;
+    }
+
+    componentDidMount() {
+        AsyncStorage.getItem('custom_key')
+            .then(value => {
+                if (value !== null) {
+                    console.log(value);
+                    this.setState({data: JSON.parse(value)});
+                }
+            })
     }
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1
+    },
+    checkbox: {
+        tintColor: '#63B8FF'
     }
 });
